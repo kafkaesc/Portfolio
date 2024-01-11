43d131a78bf98603de0a51925268e26cf95e7f07
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-11 11:34:53 -0600

    Building out responsive design for the jumbotron

--- src/components/Jhumbotron.tsx
+++ src/components/Jhumbotron.tsx
@@ -1,9 +1,16 @@
+import JhumbotronHeadshot from './JhumbotronHeadshot';
+
 export default function Jhumbotron() {
 	return (
-		<div className="text-center">
-			<h1 className="text-6xl">Jared Hettinger</h1>
-			<h2 className="text-4xl">Software Engineer</h2>
-			<p className="text-2xl">Freckled Latino</p>
+		<div className="bg-slate-200">
+			<div className="max-w-4xl px-2 mx-auto md:h-[25rem] md:relative">
+				<JhumbotronHeadshot />
+				<div className="font-bold text-center md:text-left md:pl-4 md:pt-16">
+					<h1 className="text-6xl">Jared Hettinger</h1>
+					<h2 className="text-4xl">Software Engineer</h2>
+					<p className="text-2xl">Freckled Latino</p>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/JhumbotronHeadshot.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function JhumbotronHeadshot() {
+	return (
+		<Image
+			alt="Headshot of Jared Hettinger"
+			className="w-32 h-32 p-1 mx-auto border border-neutral-950 rounded-full md:w-96 md:h-96 md:rounded-none md:border-none md:absolute md:bottom-0 md:right-0 md:mr-16 md:p-0"
+			height={384}
+			src="/images/headshot.png"
+			width={384}
+		/>
+	);
+}
