bccddd33610fb035e6cb73b2055c737fc1fb46a0
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-12 15:45:34 -0600

    Jhumbotron style updates

--- src/components/Jhumbotron.tsx
+++ src/components/Jhumbotron.tsx
@@ -3,9 +3,9 @@ import JhumbotronHeadshot from './JhumbotronHeadshot';
 export default function Jhumbotron() {
 	return (
 		<div className="bg-slate-200">
-			<div className="max-w-4xl px-2 mx-auto md:h-[25rem] md:relative">
+			<div className="max-w-4xl px-2 pt-2 mx-auto md:pt-0 md:h-[25rem] md:relative">
 				<JhumbotronHeadshot />
-				<div className="font-bold text-center md:text-left md:pl-4 md:pt-16">
+				<div className="font-bold text-center md:text-left md:pl-8 md:pt-16">
 					<h1 className="text-6xl">Jared Hettinger</h1>
 					<h2 className="text-4xl">Software Engineer</h2>
 					<p className="text-2xl">Freckled Latino</p>
--- src/components/JhumbotronHeadshot.tsx
+++ src/components/JhumbotronHeadshot.tsx
@@ -4,7 +4,7 @@ export default function JhumbotronHeadshot() {
 	return (
 		<Image
 			alt="Headshot of Jared Hettinger"
-			className="w-32 h-32 p-1 mx-auto border border-neutral-950 rounded-full md:w-96 md:h-96 md:rounded-none md:border-none md:absolute md:bottom-0 md:right-0 md:mr-16 md:p-0"
+			className="w-32 h-32 p-1 mx-auto border rounded-full border-neutral-950 md:w-96 md:h-96 md:rounded-none md:border-none md:absolute md:bottom-0 md:right-0 md:p-0"
 			height={384}
 			src="/images/headshot.png"
 			width={384}
