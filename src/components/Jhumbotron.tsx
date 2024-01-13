aa93b312f3e6a628a65ea3a7b2fe0701a07b8f6e
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-13 10:11:08 -0600

    Jhumbotron style updates

--- src/components/Jhumbotron.tsx
+++ src/components/Jhumbotron.tsx
@@ -6,9 +6,9 @@ export default function Jhumbotron() {
 			<div className="max-w-4xl px-2 pt-2 mx-auto md:pt-0 md:h-[25rem] md:relative">
 				<JhumbotronHeadshot />
 				<div className="font-bold text-center md:text-left md:pl-8 md:pt-16">
-					<h1 className="text-6xl">Jared Hettinger</h1>
-					<h2 className="text-4xl">Software Engineer</h2>
-					<p className="text-2xl">Freckled Latino</p>
+					<h1 className="text-5xl sm:text-6xl">Jared Hettinger</h1>
+					<h2 className="text-3xl sm:text-4xl">Software Engineer</h2>
+					<p className="text-xl sm:text-2xl">Freckled Latino</p>
 				</div>
 			</div>
 		</div>
