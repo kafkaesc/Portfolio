a07233615ff330543268d7c469d5ba660794a502
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-16 11:29:12 -0600

    Adding the Hr component. Updating styles on the H2 component.

--- src/elements/H2.tsx
+++ src/elements/H2.tsx
@@ -7,8 +7,10 @@ interface H2Props {
 
 export default function H2({ children, className }: H2Props) {
 	return className ? (
-		<h2 className={`text-2xl font-bold ${className}`}>{children}</h2>
+		<h2 className={`text-2xl font-bold text-jh-black ${className}`}>
+			{children}
+		</h2>
 	) : (
-		<h2 className="text-2xl font-bold">{children}</h2>
+		<h2 className="text-2xl font-bold text-jh-black">{children}</h2>
 	);
 }
