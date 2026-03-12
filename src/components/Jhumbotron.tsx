9ddf1c5c63045677c0163f7f2de7abe3f6d00ad5
kafkaesc <jared.a.hettinger@gmail.com>
2026-03-12 07:58:10 -0500

    Copy update

--- src/components/Jhumbotron.tsx
+++ src/components/Jhumbotron.tsx
@@ -4,7 +4,7 @@ import Describer from '@/components/Describer';
 export default function Jhumbotron() {
 	return (
 		<div className="bg-jh-black">
-			<div className="max-w-4xl  px-2 py-2 mx-auto md:py-0 md:h-[25rem] md:relative">
+			<div className="max-w-4xl px-2 py-2 mx-auto md:py-0 md:h-[25rem] md:relative">
 				<JhumbotronHeadshot />
 				<div className="font-black text-center text-jh-white md:text-left md:pl-8 md:pt-16">
 					<h1 className="mb-1 text-5xl sm:text-6xl">Jared Hettinger</h1>
