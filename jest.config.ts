1061a18c18dde4ffc5b63eecaac1956f7794ad92
kafkaesc <jared.a.hettinger@gmail.com>
2026-04-09 10:43:32 -0500

    Ignoring Lalo easter egg for testing

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
 	coverageProvider: 'v8',
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-	collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+	collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/app/Lalo/**'],
 	coverageThreshold: {
 		global: {
 			// TODO: Set to 0 until tests are set up
